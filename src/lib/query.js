8b2844c104886c5a880b1be7544a7c5f90aa74ea
likawoopitiki <blackalsatian@gmail.com>
2021-11-14 00:35:48 +0200

    fixed index page query.

--- src/lib/query.js
+++ src/lib/query.js
@@ -43,6 +43,32 @@ export async function getAllPageSlugs() {
   return allPagePaths
 }
 
+// get index page data
+export async function getIndexPageData() {
+  const menusData = await fetcher(ALL_MENUS)
+  const metaData = await fetcher(ALL_SITE_META)
+
+  let data = {}
+  const variables = {
+    uri: '/'
+  }
+
+  const latestPosts = await fetcher(LATEST_POSTS)
+  const pageData = await fetcher(PAGE_BY_URI, { variables })
+
+  return (data = {
+    type: 'category',
+    menus: menusData.data || {},
+    meta: metaData.data || {},
+    page: {
+      uri: pageData?.data?.page?.uri || {},
+      seo: pageData?.data?.page?.seo || {},
+      page: pageData?.data?.page || {},
+      posts: latestPosts?.data?.posts || {}
+    }
+  })
+}
+
 // get all page data
 export async function getPageData({ params: { slug } }) {
   const menusData = await fetcher(ALL_MENUS)
