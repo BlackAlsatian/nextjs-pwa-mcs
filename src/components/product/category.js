299a5a53a4c862969339195a418f727d6e447bc2
likawoopitiki <blackalsatian@gmail.com>
2022-02-17 09:38:01 +0200

    Category view.

--- src/components/product/category.js
+++ src/components/product/category.js
@@ -2,30 +2,46 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from './category.module.scss'
 
-const Category = ({ title, slug, image, uri }) => {
+const Category = ({ title, image, uri }) => {
+  const placeholderImage = {
+    altText: 'No image available',
+    sourceUrl:
+      'https://wp.motioncontrolsystems.co.za/wp-content/uploads/woocommerce-placeholder.png',
+    mediaDetails: {
+      height: 800,
+      width: 800
+    }
+  }
+  console.log('image: ', image)
+  console.log('placeholder image: ', placeholderImage)
+  const categoryImage = image || placeholderImage
+  console.log('category image: ', categoryImage)
   return (
     <div className={styles.wrapper}>
       <div className={styles.image}>
-        {image && (
-          <Image
-            width={image?.mediaDetails?.width}
-            height={image?.mediaDetails?.height}
-            alt={image?.altText}
-            src={image?.sourceUrl}
-            layout='responsive'
-            placeholder='blur'
-            blurDataURL='/images/placeholder.png'
-          />
-        )}
-      </div>
-      <h2>{title}</h2>
-      <p>slug: {slug}</p>
-      <p>
-        uri:{' '}
+        {/* {image && ( */}
         <Link href={uri}>
-          <a>{title}</a>
+          <a alt={title}>
+            <Image
+              width={categoryImage?.mediaDetails?.width}
+              height={categoryImage?.mediaDetails?.height}
+              alt={categoryImage?.altText}
+              src={categoryImage?.sourceUrl}
+              layout='responsive'
+              placeholder='blur'
+              blurDataURL='/images/placeholder.png'
+            />
+          </a>
         </Link>
-      </p>
+        {/* )} */}
+      </div>
+      <div className={styles.name}>
+        <h2>
+          <Link href={uri}>
+            <a>{title}</a>
+          </Link>
+        </h2>
+      </div>
     </div>
   )
 }
